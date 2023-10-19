f6f1aebcb1e1b9460cff068899bea58d803e3f31
unknown <seunyayu@gmail.com>
2023-10-19 14:27:14 +0100

    Updates

---
+++ precache-manifest.079ce9c7c0fa428acfc7901d724a09b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dce71f81b51d01ef9adba0ff220daa",
+    "url": "/afrima_temp/index.html"
+  },
+  {
+    "revision": "33db00ae5ff54a818ea6",
+    "url": "/afrima_temp/static/css/2.1f87a9f3.chunk.css"
+  },
+  {
+    "revision": "8014514a7bf857b16325",
+    "url": "/afrima_temp/static/css/main.36a0c9d5.chunk.css"
+  },
+  {
+    "revision": "33db00ae5ff54a818ea6",
+    "url": "/afrima_temp/static/js/2.2797f486.chunk.js"
+  },
+  {
+    "revision": "c702756c9cb0ce7eb52f",
+    "url": "/afrima_temp/static/js/3.d8ac33f5.chunk.js"
+  },
+  {
+    "revision": "8014514a7bf857b16325",
+    "url": "/afrima_temp/static/js/main.fffd7519.chunk.js"
+  },
+  {
+    "revision": "7d04b2952c066c34ebb5",
+    "url": "/afrima_temp/static/js/runtime~main.de2563a5.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/afrima_temp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/afrima_temp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/afrima_temp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/afrima_temp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ed9e140a855f93accaa80e0717ba5f7f",
+    "url": "/afrima_temp/static/media/texture.ed9e140a.jpg"
+  }
+]);
